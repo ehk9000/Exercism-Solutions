87a313c3ddb6ba50d2db4067ee6257a3592720ff
Erik Konner <erikkonner@gmail.com>
2019-08-27 11:28:37 -0600

    resistor color duo

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const value = (colors) => {
+  const colorList = colors.map(color => {
+    
+  });
+
+};
