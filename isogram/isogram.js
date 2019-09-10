4d367752ab6d484c7ace12f698c59890532b9c1c
Erik Konner <erikkonner@gmail.com>
2019-09-10 15:12:05 -0600

    Isogram

---
+++ isogram/isogram.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Isogram' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const isIsogram = (string) => {
+  const stringArray = string.toLowerCase().split('');
+  console.log(stringArray)
+  stringArray.map(letter => {
+    if ()
+  });
+};
