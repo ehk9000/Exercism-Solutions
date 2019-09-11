4e937a70c909c3028d2d2e34d1cca25062460784
Erik Konner <erikkonner@gmail.com>
2019-09-10 18:27:25 -0600

    Grade-school solution

--- grade-school/grade-school.js
+++ grade-school/grade-school.js
@@ -4,15 +4,32 @@
 //
 
 export class GradeSchool {
+  // add constructor for state 
+  constructor() {
+    this.currentRoster = {}
+  }
+
   roster() {
-    return {}
+    // until second to last test, had return this.currentRoster
+    return JSON.parse(JSON.stringify(this.currentRoster))
   }
 
-  add() {
-
+  add(name, grade) {
+    // check if grade already exists. 
+    if (!Object.keys(this.currentRoster).includes(grade.toString())) {
+      // if it doesn't, make new key value with grade and array of name 
+      this.currentRoster[grade] = [name];
+    } else {
+      // if it does exist, go to that grade and push name into array 
+      this.currentRoster[grade].push(name)
+    }
+    this.grade(grade)
   }
 
-  grade() {
-
+  grade(grade) {
+    if (!this.currentRoster[grade]) {
+      return []
+    }
+    return JSON.parse(JSON.stringify(this.currentRoster[grade].sort()))
   }
 }
