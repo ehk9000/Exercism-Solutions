f3bf4f22724726064ef828454be8ff8db0eabea6
Erik Konner <erikkonner@gmail.com>
2019-09-12 13:36:12 -0600

    Matrix Problem

---
+++ matrix/matrix.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Matrix {
+  constructor() {
+    
+  }
+
+  get rows() {
+    
+  }
+
+  get columns() {
+    
+  }
+}
